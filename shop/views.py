10b4f2e6a7d26db7ad600d8cfc40ee05a8c2fc3a
clever-marcus <71174834+clever-marcus@users.noreply.github.com>
2022-02-24 08:42:24 +0300

    first commit

---
+++ shop/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def store(request):
+    context = {}
+    return render(request, 'shop/store.html', context)
+
+def cart(request):
+    context = {}
+    return render(request, 'shop/cart.html', context)
+
+def checkout(request):
+    context = {} 
+    return render(request, 'shop/checkout.html', context)
